c8d0c9def992077d8500531171ab8abfe62545e1
Synopsik <synopsikk@gmail.com>
2025-05-16 12:25:47 -0700

    Chapter 1 | Additional comments

--- Chapter1/Strings.cs
+++ Chapter1/Strings.cs
@@ -6,6 +6,7 @@ public class Strings
      * A string can be set to a sequence of characters, a single char, an empty string, and null
      * Concatenation x + y is how you can perform operations on strings, duh!
      * You can access a character in a string using []
+     * True will return from comparisons (==) if they contain the same sequence of characters
      */
     
     public static void StringsDemo()
@@ -26,17 +27,23 @@ public class Strings
         string note2 = $"{firstName}{lastName} started programming in {year}";
         Console.WriteLine(note2);
         
+        // Init names
         string[] names = ["Wade", "Frank", "Edward"];
+        
+        // Init dates
         DateTime[] dates = [
             new(1988, 11, 9), 
             new(1995, 4, 25), 
             new(2003, 7, 23)
         ];
+        
+        // Init temps
         float[] temperatures = [36.6f, 39.1f, 35.9f];
         
         // Using interpolated strings, we also have options for alignment and formatting
         Console.WriteLine($"{"Name", -8} {"Birth date", 10} {"Temp. [C]", 11} -> Result");
-
+        
+        // Print results
         for (int i = 0; i < names.Length; i++)
         {
             string line = $"{names[i],-8} " +
