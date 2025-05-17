cd9b0be7bd7716854c3dde66e8765318e4fe4b34
Synopsik <synopsikk@gmail.com>
2025-05-16 21:57:03 -0700

    Added nullable reference, end-of-chapter section
    Touching back on the nullable reference types. We create a nullable list that collects measurements that are right 2/3 the time. Testing out ternary operators with a validator method

--- Chapter1/Nullable.cs
+++ Chapter1/Nullable.cs
@@ -1,10 +1,12 @@
-﻿namespace Chapter1;
+﻿using System.Diagnostics.Metrics;
+
+namespace Chapter1;
 
 
 // ------------------------------------------------------------------------------------------------ //
 //                                         Nullable                                                 //
 // ------------------------------------------------------------------------------------------------ //
-public static class Nullable
+public static class NullableDemo
 {
     public static void NullableTesting()
     {
@@ -50,3 +52,42 @@ public static class Nullable
         // 364 / 365 would equal 0 using integer division
     }
 }
+
+
+// ------------------------------------------------------------------------------------------------ //
+//                              End of Chapter - Nullable reference types                           //
+// ------------------------------------------------------------------------------------------------ //
+public class NullableRef
+{
+    public async static void NullableRefDemo()
+    {
+        Random random = new();
+        List<Measurement?> measurements = [];
+        
+        for (int i = 0; i < 100; i++)
+        {
+            Measurement? measurement = random.Next(3) != 0
+                       ? new(DateTime.Now, random.Next(1000) / 1000.0f)
+                       : null;
+            measurements.Add(measurement);
+            
+            // Ternary operator, I need to remember these,
+            // condition ? valueIfTrue : valueIfFalse
+            Console.WriteLine(IsValid(measurement) ? measurement!.ToString() : "-");
+            await Task.Delay(100);
+        }
+
+        static bool IsValid(Measurement? measurement)
+        {
+            // In a way, this eliminates the try/catch block since if measurement is invalid/null/etc we return false
+            // Else, if measurement is between 0-1, return True 
+            return measurement != null
+                   && measurement.Value.MeasValue >= 0.0f
+                   && measurement.Value.MeasValue <= 1.0f;
+            // OR return measurement is Measurement { MeasValue: >= 0.0f and <= 1.0f };
+            // Is that enough to check for validity?
+        }
+    }
+}
+
+
