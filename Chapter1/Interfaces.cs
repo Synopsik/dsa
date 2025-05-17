4b675d3b7ab984692b8086f7380cf4afca832b23
Synopsik <synopsikk@gmail.com>
2025-05-16 20:08:05 -0700

    Created Interfaces
    Example device interface used with demo class for mock tech device.

---
+++ Chapter1/Interfaces.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter1;
+
+public class Interfaces
+{
+    public static void InterfacesDemo()
+    {
+        Classes.FutureDevice demo = new Classes.FutureDevice()
+        {
+            Id = 24,
+            Model = "XYZ22",
+            Year = 2025
+        };
+        Console.WriteLine($"{"ID",-5} {"Model",10} {"Year",11}");
+        Console.WriteLine($"{demo.Id,-5} {demo.Model,10} {demo.Year,11}");
+    }
+}
+
+public interface IDevice
+{
+    int Id { get; set; }
+    string Model { get; set; }
+    int Year { get; set; }
+    void Configure(Classes.DeviceConfiguration configuration);
+    bool Start();
+    bool Stop();
+}
