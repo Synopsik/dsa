e6c54f5daeb8611a10932d3562df73f9053ff686
Synopsik <synopsikk@gmail.com>
2025-05-16 20:09:32 -0700

    Created Dynamics
    Still don't fully understand this, need to document more, and I'll update with a better understanding.

---
+++ Chapter1/Dynamics.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter1;
+
+public class Dynamics
+{
+    public async Task DynamicsAsync()
+    {
+        dynamic posts = await GetPostsAsync();
+        foreach (dynamic post in posts)
+        {
+            string title = post.title;
+            string body = post.body;
+            Console.WriteLine($"Title: {title}\n" +
+                              $"Body:  {body}\n");
+        }
+    }
+
+    Task<dynamic> GetPostsAsync()
+    {
+        var libraryTitles = new[]
+        {
+            new { title = "Strange Stories", body = "A collection of strange stories" },
+            new { title = "Academic Intrests", body = "A collection a noteworthy academic novels" }
+        };
+        return Task.FromResult<dynamic>(libraryTitles);
+    }
+}
