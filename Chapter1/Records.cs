84c7203310a67b1aac374de058a23981ff1fea69
Synopsik <synopsikk@gmail.com>
2025-05-16 20:05:26 -0700

    Ditching Chapter 1 convention & finishing Records
    Redundant visually, going to put short bite size info on what major changes were and then here I can place more info if needed. Also I finished the Records section.

--- Chapter1/Records.cs
+++ Chapter1/Records.cs
@@ -1,11 +1,57 @@
 ﻿namespace Chapter1;
 
+// ------------------------------------------------------------------------------------------------ //
+//                                          Records                                                 //
+// ------------------------------------------------------------------------------------------------ //
+/*
+ * Records have inherited functionality that allows equality-based comparisons
+ * They differ from a class, when two records are compared they are only equal
+ * if they are references the same data
+ * Keywords include: record
+ *                   record class
+ * 
+ */
+
+
+public record Ant(string Name, string Type, int Height, float Weight, int Age);
+
+public record Robot
+{
+    public required string Name  { get; set; }
+    public required string Job   { get; set; }
+    public required int Height   { get; set; }
+    public required float Weight { get; set; }
+    public required int Lifespan { get; set; }
+}
+
+
 public class Records
 {
-    // ------------------------------------------------------------------------------------------------ //
-    //                                          Records                                                 //
-    // ------------------------------------------------------------------------------------------------ //
-    /*
-     * 
-     */
+    public static void RecordsDemo()
+    {
+        // A primary constructor automatically creates properties for you
+        Ant ant = new Ant("Ted", "Fire", 1, 0.1f, 1);
+        Console.WriteLine(ant);
+
+        // ant.Name = "Fred"; Causes errors, can't alter record after init
+        
+        // Robot Record isn't using a primary constructor;
+        // therefore, explicit properties were marked with required and must be labeled
+        Robot robot = new Robot
+        {
+            Name     = "C348",
+            Job      = "Firehydrant",
+            Height   = 2000,
+            Weight   = 300.2f,
+            Lifespan = 999999
+        };
+        Console.WriteLine(robot);
+        robot.Name = "T999"; // Can change the name of this record
+        Console.WriteLine(robot);
+
+        // non-destructive mutation
+        Robot paperweight = robot with { Name = "Beep", Height = 9999 };
+        Console.WriteLine(paperweight);
+        
+    }
 }
