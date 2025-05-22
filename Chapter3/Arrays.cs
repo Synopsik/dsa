a3602f30df3e49cffed4b118e42c7330a1f46b71
steven <synopsikk@gmail.com>
2025-05-21 23:07:35 -0700

    Added multi-dim arrays

--- Chapter3/Arrays.cs
+++ Chapter3/Arrays.cs
@@ -97,4 +97,50 @@ public class Arrays
 
     }
     
+    // ------------------------------------------------------------------------------------------------ //
+    //                                          2D Arrays                                               //
+    // ------------------------------------------------------------------------------------------------ //
+    public static void TwoD_Demo()
+    {
+        // Create a two-dimensional array
+        int[,] numbers = new int[10, 5];
+
+        // Find the end index for each dimension
+        var dimOneEnd = numbers.GetLength(0) - 1;
+        var dimTwoEnd = numbers.GetLength(1) - 1;
+        
+        numbers[dimOneEnd, dimTwoEnd] = 17;
+        
+        // We can also combine a declaration with an initialization
+        var numbers2 = new int[,]
+        {
+            { 9, 5, -9 },
+            { -11, 4, 0 },
+            { 6, 115, 3 },
+            { -12, -9, 71 },
+            { 1, -6, -1 }
+        };
+
+        // We use the same method to access elements from the array
+
+        int element = numbers2[2, 1];
+        numbers2[1, 0] = 11;
+
+    }
+    
+    // ------------------------------------------------------------------------------------------------ //
+    //                                       Multi-D Arrays                                             //
+    // ------------------------------------------------------------------------------------------------ //
+
+    public static void threeD_Demo()
+    {
+        // Create 3-dimensional array
+        var numbers3 = new int[3, 2, 3];
+    }
+
+    public static void fourD_Demo()
+    {
+        // Create 4-dimensional array
+        var numbers4 = new int[5, 4, 3, 2];
+    }
 }
